84d4c6fb0d89f9d97e8fec43615d01c89868c855
MaksimMyson <maksimmyson33@gmail.com>
2024-03-05 15:26:43 +0200

    exe1

--- CSharp6_05_03/Program.cs
+++ CSharp6_05_03/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IOutput output = new ConsoleOutput();
+
+            output.Show(); 
+            output.Show("Custom message"); 
         }
+    }
+}
+public interface IOutput
+{
+    void Show();
+    void Show(string info);
+}
+
+public class ConsoleOutput : IOutput
+{
+    public void Show()
+    {
+        Console.WriteLine("Default information");
+    }
+
+    public void Show(string info)
+    {
+        Console.WriteLine($"Information: {info}");
     }
 }
